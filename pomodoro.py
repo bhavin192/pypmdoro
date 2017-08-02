2b0266c6a560d66e89862f18b61b5d516ec8a303
Bhavin Gandhi <bhavin7392@gmail.com>
2017-08-02 23:35:05 +0530

    Adds two modes continuous and per cycle continuous will keep the cycle running util you stop it, and per cycle mode will ask to continue after every cycle.
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- pomodoro.py
+++ pomodoro.py
@@ -12,8 +12,19 @@ def send_notification(message):
 work_time = int(input("Enter work duration in minutes: "))
 break_time = int(input("Enter break duration in minutes: "))
 
+mode = int(input("""Please select the mode,
+        1. Continuous mode - script will keep runnig until you stop it.
+        2. Per cycle mode - script will ask to continue after every cycle.
+        """))
+per_cycle_mode_choice = "Y"
+
 while True:
     send_notification("'Start! Focus on your work.'")
     time.sleep(work_time * 60)
     send_notification("'Good job! Take a break.'")
     time.sleep(break_time * 60)
+    if mode == 2:
+        per_cycle_mode_choice =  input("Do you want to continue? [Y/n]") 
+        if per_cycle_mode_choice != "Y" and per_cycle_mode_choice != "y":
+            break
+        
