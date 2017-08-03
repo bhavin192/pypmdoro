fbc855728de2c95ee3577c45654d02f4da46794a
Bhavin Gandhi <bhavin7392@gmail.com>
2017-08-03 20:56:34 +0530

    Import only sleep and system from time and os
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- pomodoro.py
+++ pomodoro.py
@@ -1,13 +1,13 @@
 #!/usr/share/env python3
-import os
-import time
+from os import system
+from time import sleep
 
 def send_notification(message):
     """ Sends notification with given string as content using notify-send command
     :param message: string containing the notification content
     :returns: None"""
 
-    os.system("notify-send " + message)
+    system("notify-send " + message)
 
 work_time = int(input("Enter work duration in minutes: "))
 break_time = int(input("Enter break duration in minutes: "))
@@ -20,9 +20,9 @@ per_cycle_mode_choice = "Y"
 
 while True:
     send_notification("'Start! Focus on your work.'")
-    time.sleep(work_time * 60)
+    sleep(work_time * 60)
     send_notification("'Good job! Take a break.'")
-    time.sleep(break_time * 60)
+    sleep(break_time * 60)
     if mode == 2:
         per_cycle_mode_choice =  input("Do you want to continue? [Y/n]") 
         if per_cycle_mode_choice != "Y" and per_cycle_mode_choice != "y":
